b6d38e5d9bbf2ef80d988f40b5de35fe5b3f9628
GiGi <gigi@hurtault.dev>
2023-11-29 23:35:15 -0500

    Rework to take command line arg and add ByteSize package

--- learning-experiments.fsx
+++ learning-experiments.fsx
@@ -1,19 +1,14 @@
+#r "nuget: ByteSize, 2.1.1" //add ByteSize package reference
 open System
 open System.IO
 
-let path = "/home/gigi/Downloads"
+//how to take in a commandline arg?
+let main (args) =
+    let path = args
+    let pathInfo = FileInfo(path)
+    let files = Directory.EnumerateFiles(path) //EnumerateFiles vs GetFiles?
 
-let pathInfo = FileInfo(path)
+    //add up sizes and print
+    files |> List.ofSeq |> List.map (fun f -> FileInfo(f).Length) |> List.sum |> printf "the collective file size in the directory %s is %d" path
 
-//printf "the last modified time is %A" pathInfo.LastAccessTime //1st test
-
-//enumerate files in a folder
-let files = Directory.EnumerateFiles(path) //EnumerateFiles vs GetFiles?
-//yields a seq with strings of file paths for files
-
-//get file sizes for all files in folder
-files |> List.ofSeq |> List.map (fun f -> FileInfo(f).Length)
-//yields val it: int64 list = [0L; 0L; 0L; 0L; 0L; 0L; 0L; 0L; 0L; 0L; 25L; 10L]
-
-//add up sizes and print
-files |> List.ofSeq |> List.map (fun f -> FileInfo(f).Length) |> List.sum |> printf "the collective file size in the directory %s is %d" path
+main fsi.CommandLineArgs[0] //add 'from fsi' when running script
